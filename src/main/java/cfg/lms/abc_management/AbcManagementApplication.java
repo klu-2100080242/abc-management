5a014fc528e03598f7f0285e0588777d877c5179
klu-2100080242 <2100080242@kluniversity.in>
2025-07-17 11:38:41 +0530

    commit

---
+++ src/main/java/cfg/lms/abc_management/AbcManagementApplication.java
@@ -0,0 +1,13 @@
+package cfg.lms.abc_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcManagementApplication.class, args);
+	}
+
+}
